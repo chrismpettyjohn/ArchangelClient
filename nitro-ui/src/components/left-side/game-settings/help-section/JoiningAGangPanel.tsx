c3f87b4e9f17d1fec32c6cf37b9f4fe6e77480af
chrismpettyjohn <chrismpettyjohn@gmail.com>
2024-10-30 14:14:12 -0400

    feat: joining gang

--- nitro-ui/src/components/left-side/game-settings/help-section/JoiningAGangPanel.tsx
+++ nitro-ui/src/components/left-side/game-settings/help-section/JoiningAGangPanel.tsx
@@ -1,10 +1,34 @@
 import { Text } from "../../../../common";
+import { FaUserFriends, FaSkullCrossbones, FaMoneyBillWave, FaBolt } from "react-icons/fa";
 
 export function JoiningAGangPanel() {
     return (
         <>
-            <Text bold fontSize={4} variant="white">Join a gang</Text>
-            <p> u sure u wanna do that</p>
+            <Text bold fontSize={4} variant="white">Join a Gang</Text>
+            <p>Joining a gang in Nueva Esperanza can offer protection, profits, and a place to belong, but it’s a dangerous path.</p>
+            <br />
+
+            <Text bold fontSize={4} variant="white">Gang Benefits</Text>
+            <div>
+                <FaUserFriends style={{ marginRight: 8 }} />
+                <Text fontSize={4} variant="white">Allies = Gain protection and backup from gang members</Text>
+            </div>
+            <div>
+                <FaMoneyBillWave style={{ marginRight: 8 }} />
+                <Text fontSize={4} variant="white">Income = Earn from gang activities and share in profits</Text>
+            </div>
+            <br />
+
+            <Text bold fontSize={4} variant="white">Risks</Text>
+            <div>
+                <FaSkullCrossbones style={{ marginRight: 8 }} />
+                <Text fontSize={4} variant="white">Violence = Be prepared for dangerous confrontations</Text>
+            </div>
+            <div>
+                <FaBolt style={{ marginRight: 8 }} />
+                <Text fontSize={4} variant="white">Wanted Level = Increased attention from law enforcement</Text>
+            </div>
+            <p>Consider the costs carefully before joining a gang; once in, it's tough to get out.</p>
         </>
-    )
+    );
 }
