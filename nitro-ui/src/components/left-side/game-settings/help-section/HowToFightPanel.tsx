dae5f7126ce96875a70861513a364648222b8a96
chrismpettyjohn <chrismpettyjohn@gmail.com>
2024-10-30 14:13:18 -0400

    feat: making money

--- nitro-ui/src/components/left-side/game-settings/help-section/HowToFightPanel.tsx
+++ nitro-ui/src/components/left-side/game-settings/help-section/HowToFightPanel.tsx
@@ -1,10 +1,42 @@
+import { FaCrosshairs, FaFistRaised, FaHeart, FaShieldAlt, FaSyncAlt } from "react-icons/fa";
 import { Text } from "../../../../common";
 
 export function HowToFightPanel() {
     return (
         <>
-            <Text bold fontSize={4} variant="white">How to fight</Text>
-            <p> u sure u wanna do that</p>
+            <Text bold fontSize={4} variant="white">How to Fight</Text>
+            <p>
+                Fighting in Nueva Esperanza requires skill and strategy. Here are the essentials to get you started:
+            </p>
+            <br />
+            <Text bold fontSize={4} variant="white">Combat Basics</Text>
+            <div>
+                <FaFistRaised style={{ marginRight: 8 }} />
+                <Text fontSize={4} variant="white">i = Toggle Combat Mode</Text>
+            </div>
+            <div>
+                <FaCrosshairs style={{ marginRight: 8 }} />
+                <Text fontSize={4} variant="white">Left Click = Attack</Text>
+                <Text fontSize={5} style={{ marginLeft: 20 }}>*Must be in combat mode</Text>
+            </div>
+            <div>
+                <FaShieldAlt style={{ marginRight: 8 }} />
+                <Text fontSize={4} variant="white">hold Shift = Block/Defend</Text>
+            </div>
+            <div>
+                <FaSyncAlt style={{ marginRight: 8 }} />
+                <Text fontSize={4} variant="white">r = Reload</Text>
+            </div>
+            <br />
+            <Text bold fontSize={4} variant="white">Health & Healing</Text>
+            <div>
+                <FaHeart style={{ marginRight: 8 }} />
+                <Text fontSize={4} variant="white">h = Use Medkit</Text>
+            </div>
+            <br /><br />
+            <p>
+                Remember, once in combat mode, you’re visible as a combatant, so stay aware of your surroundings and always be prepared!
+            </p>
         </>
-    )
+    );
 }
--- nitro-ui/src/components/left-side/game-settings/help-section/MakingMoneyPanel.tsx
+++ nitro-ui/src/components/left-side/game-settings/help-section/MakingMoneyPanel.tsx
@@ -1,10 +1,43 @@
 import { Text } from "../../../../common";
+import { FaTractor, FaFish, FaTree, FaHardHat, FaUserSecret, FaDollarSign } from "react-icons/fa";
 
 export function MakingMoneyPanel() {
     return (
         <>
-            <Text bold fontSize={4} variant="white">Making money</Text>
-            <p>u sure u wanna do that</p>
+            <Text bold fontSize={4} variant="white">Making Money</Text>
+            <p>In Nueva Esperanza, earning a living can be honest, risky, or a mix of both. Choose your path wisely:</p>
+            <br />
+
+            <Text bold fontSize={4} variant="white">Legitimate Work</Text>
+            <div>
+                <FaHardHat style={{ marginRight: 8 }} />
+                <Text fontSize={4} variant="white">Jobs = Find stable work for steady income</Text>
+            </div>
+            <div>
+                <FaTractor style={{ marginRight: 8 }} />
+                <Text fontSize={4} variant="white">Farming = Grow and sell crops</Text>
+            </div>
+            <div>
+                <FaFish style={{ marginRight: 8 }} />
+                <Text fontSize={4} variant="white">Fishing = Catch and sell fish at markets</Text>
+            </div>
+            <div>
+                <FaTree style={{ marginRight: 8 }} />
+                <Text fontSize={4} variant="white">Tree Chopping = Harvest wood for profit</Text>
+            </div>
+            <div>
+                <FaDollarSign style={{ marginRight: 8 }} />
+                <Text fontSize={4} variant="white">Mining = Extract valuable resources to sell</Text>
+            </div>
+
+            <br />
+            <Text bold fontSize={4} variant="white">Criminal Paths</Text>
+            <div>
+                <FaUserSecret style={{ marginRight: 8 }} />
+                <Text fontSize={4} variant="white">Robbery = Target individuals or businesses</Text>
+            </div>
+            <br /><br /><br />
+            <p>Choose wisely between honest and criminal paths; each comes with its own risks and rewards.</p>
         </>
-    )
+    );
 }
