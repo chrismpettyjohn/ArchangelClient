01331bea506b4a1746b34c937878daa665b36467
chrismpettyjohn <chrismpettyjohn@gmail.com>
2024-11-03 21:56:33 -0500

    fix: clean up

--- nitro-ui/src/components/mod-tools/views/roleplay/editors/CrimeEditor.tsx
+++ nitro-ui/src/components/mod-tools/views/roleplay/editors/CrimeEditor.tsx
@@ -1,6 +1,6 @@
 import { CrimeData } from "@nitro-rp/renderer";
-import { Grid, Text } from "../../../../../common";
-import { ChangeEvent, SyntheticEvent, useCallback, useState } from "react";
+import { Text } from "../../../../../common";
+import { SyntheticEvent, useCallback, useState } from "react";
 import { Button } from "react-bootstrap";
 import { FaPencilAlt } from "react-icons/fa";
 
@@ -14,7 +14,7 @@ export function CrimeEditor({ defaultCrime, onSave }: CrimeEditorProps) {
         id: defaultCrime?.id ?? -1,
         displayName: defaultCrime?.displayName ?? '',
         description: defaultCrime?.description ?? '',
-        jailTime: defaultCrime?.jailTime ?? 60,
+        jailTime: Math.round(defaultCrime?.jailTime / 60) ?? 2,
     });
 
     const onChange = useCallback((data: Partial<CrimeData>) => {
@@ -26,7 +26,10 @@ export function CrimeEditor({ defaultCrime, onSave }: CrimeEditorProps) {
 
     const onSubmit = useCallback((event: SyntheticEvent) => {
         event.preventDefault();
-        onSave(crime);
+        onSave({
+            ...crime,
+            jailTime: Math.round(crime.jailTime * 60)
+        });
     }, [crime, onSave]);
 
     return (
@@ -34,14 +37,14 @@ export function CrimeEditor({ defaultCrime, onSave }: CrimeEditorProps) {
             <form onSubmit={onSubmit} style={{ display: 'flex', flex: 1, flexDirection: 'column', gap: 8 }}>
                 <div>
                     <Text bold fontSize={5} variant="white">Display Name</Text>
-                    <input className="form-control form-control-sm" placeholder="hk-mp5" type="text" name="displayName" value={crime.displayName} onChange={e => onChange({ displayName: e.currentTarget.value })} />
+                    <input className="form-control form-control-sm" placeholder="Murder" type="text" name="displayName" value={crime.displayName} onChange={e => onChange({ displayName: e.currentTarget.value })} />
                 </div>
                 <div>
                     <Text bold fontSize={5} variant="white">Description</Text>
-                    <textarea className="form-control form-control-sm" placeholder="HK MP5" rows={10} name="description" value={crime.description} onChange={e => onChange({ description: e.currentTarget.value })} />
+                    <textarea className="form-control form-control-sm" placeholder="Killing another civilian in a non self-defense scenario" rows={10} name="description" value={crime.description} onChange={e => onChange({ description: e.currentTarget.value })} />
                 </div>
                 <div>
-                    <Text bold fontSize={5} variant="white">Jail Time seconds</Text>
+                    <Text bold fontSize={5} variant="white">Jail time minutes</Text>
                     <input className="form-control form-control-sm" placeholder="5" type="number" name="jailTime" value={crime.jailTime} onChange={e => onChange({ jailTime: Number(e.currentTarget.value) })} />
                 </div>
             </form>
