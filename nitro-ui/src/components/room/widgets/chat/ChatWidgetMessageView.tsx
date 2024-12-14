bbe08703ee7943e84c2001a34fccc76f43c46c52
chrismpettyjohn <chrismpettyjohn@gmail.com>
2024-12-14 03:46:53 -0500

    design: improve chat styloe

--- nitro-ui/src/components/room/widgets/chat/ChatWidgetMessageView.tsx
+++ nitro-ui/src/components/room/widgets/chat/ChatWidgetMessageView.tsx
@@ -2,24 +2,20 @@ import { RoomChatSettings, RoomObjectCategory } from '@nitro-rp/renderer';
 import { FC, useEffect, useMemo, useRef, useState } from 'react';
 import { ChatBubbleMessage, GetRoomEngine } from '../../../../api';
 
-interface ChatWidgetMessageViewProps
-{
+interface ChatWidgetMessageViewProps {
     chat: ChatBubbleMessage;
     makeRoom: (chat: ChatBubbleMessage) => void;
     bubbleWidth?: number;
 }
 
-export const ChatWidgetMessageView: FC<ChatWidgetMessageViewProps> = props =>
-{
+export const ChatWidgetMessageView: FC<ChatWidgetMessageViewProps> = props => {
     const { chat = null, makeRoom = null, bubbleWidth = RoomChatSettings.CHAT_BUBBLE_WIDTH_NORMAL } = props;
-    const [ isVisible, setIsVisible ] = useState(false);
-    const [ isReady, setIsReady ] = useState<boolean>(false);
+    const [isVisible, setIsVisible] = useState(false);
+    const [isReady, setIsReady] = useState<boolean>(false);
     const elementRef = useRef<HTMLDivElement>();
 
-    const getBubbleWidth = useMemo(() =>
-    {
-        switch(bubbleWidth)
-        {
+    const getBubbleWidth = useMemo(() => {
+        switch (bubbleWidth) {
             case RoomChatSettings.CHAT_BUBBLE_WIDTH_NORMAL:
                 return 350;
             case RoomChatSettings.CHAT_BUBBLE_WIDTH_THIN:
@@ -27,15 +23,14 @@ export const ChatWidgetMessageView: FC<ChatWidgetMessageViewProps> = props =>
             case RoomChatSettings.CHAT_BUBBLE_WIDTH_WIDE:
                 return 2000;
         }
-    }, [ bubbleWidth ]);
+    }, [bubbleWidth]);
 
-    useEffect(() =>
-    {
+    useEffect(() => {
         setIsVisible(false);
-        
+
         const element = elementRef.current;
 
-        if(!element) return;
+        if (!element) return;
 
         const width = element.offsetWidth;
         const height = element.offsetHeight;
@@ -43,50 +38,47 @@ export const ChatWidgetMessageView: FC<ChatWidgetMessageViewProps> = props =>
         chat.width = width;
         chat.height = height;
         chat.elementRef = element;
-        
+
         let left = chat.left;
         let top = chat.top;
 
-        if(!left && !top)
-        {
+        if (!left && !top) {
             left = (chat.location.x - (width / 2));
             top = (element.parentElement.offsetHeight - height);
-            
+
             chat.left = left;
             chat.top = top;
         }
 
         setIsReady(true);
 
-        return () =>
-        {
+        return () => {
             chat.elementRef = null;
 
             setIsReady(false);
         }
-    }, [ chat ]);
+    }, [chat]);
 
-    useEffect(() =>
-    {
-        if(!isReady || !chat || isVisible) return;
-        
-        if(makeRoom) makeRoom(chat);
+    useEffect(() => {
+        if (!isReady || !chat || isVisible) return;
+
+        if (makeRoom) makeRoom(chat);
 
         setIsVisible(true);
-    }, [ chat, isReady, isVisible, makeRoom ]);
+    }, [chat, isReady, isVisible, makeRoom]);
 
     return (
-        <div ref={ elementRef } className={ `bubble-container ${ isVisible ? 'visible' : 'invisible' }` } onClick={ event => GetRoomEngine().selectRoomObject(chat.roomId, chat.senderId, RoomObjectCategory.UNIT) }>
-            { (chat.styleId === 0) &&
-                <div className="user-container-bg" style={ { backgroundColor: chat.color } } /> }
-            <div className={ `chat-bubble bubble-${ chat.styleId } type-${ chat.type }` } style={ { maxWidth: getBubbleWidth } }>
+        <div ref={elementRef} className={`bubble-container ${isVisible ? 'visible' : 'invisible'}`} onClick={event => GetRoomEngine().selectRoomObject(chat.roomId, chat.senderId, RoomObjectCategory.UNIT)}>
+            {(chat.styleId === 0) &&
+                <div className="user-container-bg" style={{ backgroundColor: chat.color }} />}
+            <div className={`chat-bubble bubble-${chat.styleId} type-${chat.type}`} style={{ maxWidth: getBubbleWidth }}>
                 <div className="user-container">
-                    { chat.imageUrl && (chat.imageUrl.length > 0) &&
-                        <div className="user-image" style={ { backgroundImage: `url(${ chat.imageUrl })` } } /> }
+                    {chat.imageUrl && (chat.imageUrl.length > 0) &&
+                        <div className="user-image" style={{ backgroundImage: `url(${chat.imageUrl})`, height: 75 }} />}
                 </div>
                 <div className="chat-content">
-                    <b className="username mr-1" dangerouslySetInnerHTML={ { __html: `${ chat.username }: ` } } />
-                    <span className="message" dangerouslySetInnerHTML={ { __html: `${ chat.formattedText }` } } />
+                    <b className="username mr-1" dangerouslySetInnerHTML={{ __html: `${chat.username}: ` }} style={{ fontSize: 16 }} />
+                    <span className="message" dangerouslySetInnerHTML={{ __html: `${chat.formattedText}` }} />
                 </div>
                 <div className="pointer" />
             </div>
