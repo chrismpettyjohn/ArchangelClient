46707b7cf0039058b1ef74f72596d41dd6e62025
chrismpettyjohn <chrismpettyjohn@gmail.com>
2024-12-17 21:15:51 -0500

    feat: change banner

--- nitro-ui/src/components/roleplay-widgets/community-manager/user/user-profile-editor/UserProfileEditor.tsx
+++ nitro-ui/src/components/roleplay-widgets/community-manager/user/user-profile-editor/UserProfileEditor.tsx
@@ -1,15 +1,33 @@
-import { Button, Flex } from "../../../../../common";
+import { Button, Flex, Text } from "../../../../../common";
 import { FaCaretLeft, FaPencilAlt } from "react-icons/fa";
-import { CreateLinkEvent } from "../../../../../api";
+import { CreateLinkEvent, SendMessageComposer } from "../../../../../api";
 import { CommunityLayout, useCommunityLinkTracker } from "../../CommunityLayout";
 import { _setVisible } from "ag-grid-community";
 import { useRoleplayStats } from "../../../../../hooks/roleplay/use-rp-stats";
 import { useSessionInfo } from "../../../../../hooks";
+import { ChangeEvent, SyntheticEvent, useCallback, useEffect, useState } from "react";
+import { useProfileBanner } from "../../../../../hooks/roleplay/user-profile-banner";
+import { UserChangeProfileBannerComposer } from "@nitro-rp/renderer";
 
 export function UserProfileEditor() {
     const session = useSessionInfo();
     const { active, resourceID, onHide } = useCommunityLinkTracker('users', 'profile-edit');
     const rpStats = useRoleplayStats(resourceID);
+    const banner = useProfileBanner(resourceID);
+    const [newBanner, setNewBanner] = useState(banner)
+
+    useEffect(() => {
+        setNewBanner(banner);
+    }, [banner]);
+
+    const onChangeBanner = useCallback((event: ChangeEvent<HTMLInputElement>) => {
+        setNewBanner(event.currentTarget.value)
+    }, [setNewBanner]);
+
+    const onUpdateBanner = useCallback((event: SyntheticEvent) => {
+        event.preventDefault();
+        SendMessageComposer(new UserChangeProfileBannerComposer(newBanner))
+    }, [newBanner]);
 
     if (!active) {
         return null;
@@ -22,16 +40,20 @@ export function UserProfileEditor() {
                     <FaCaretLeft style={{ marginRight: 8 }} />
                     Go back
                 </Button>
+            </Flex>
+            <form onSubmit={onUpdateBanner} style={{ display: 'flex', flex: 1, flexDirection: 'column', gap: 12 }}>
+                <Text bold fontSize={4}>Profile Banner</Text>
+                <Text fontSize={6}>imgur links only.  you can upload an image to imgur, right click and click "copy to address"</Text>
+                <input className="form-input" value={newBanner} onChange={onChangeBanner} />
                 {
                     rpStats.userID === session?.userInfo?.userId && (
-                        <Button variant="success" onClick={() => CreateLinkEvent('community/users/list')}>
+                        <Button variant="success" onClick={onUpdateBanner}>
                             <FaPencilAlt style={{ marginRight: 8 }} />
-                            Save Changes
+                            Update Banner
                         </Button>
                     )
                 }
-            </Flex>
-            woo
+            </form>
         </CommunityLayout>
     )
 }
